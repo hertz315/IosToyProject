f0d96b1e4708ed2a07eb5d0ad2f2ca1c226bb02e
hertz315 <hertz315@gmail.com>
2022-07-07 21:44:09 +0900

    MyFirstApp update

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -12,10 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel!
     
+    @IBOutlet weak var myButton: UIButton!
     
+    
+    // 앱을 실행시키면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainLabel.textColor = UIColor.yellow
+        mainLabel.text = "하이루"
     }
 
     
@@ -24,6 +29,7 @@ class ViewController: UIViewController {
         mainLabel.textColor = UIColor.red
         mainLabel.backgroundColor = #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
         
+        myButton.setTitleColor(UIColor.black, for: .normal)
     }
 }
 
